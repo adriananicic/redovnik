655eec99ba0285a5ae9dd18623e6aaa46b6db789
Adrian <adriananicic@gmail.com>
2025-05-28 22:23:09 +0200

    make serverless

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import queueRouter from "./routes/queue.routes";
-import authRouter from "./routes/auth.routes";
-import adminRouter from "./routes/admin.routes";
-import { initSocket } from "./socket";
+import queueRouter from "../routes/queue.routes";
+import authRouter from "../routes/auth.routes";
+import adminRouter from "../routes/admin.routes";
+import { initSocket } from "../socket";
 import http from "http";
-import ticketRouter from "./routes/ticket.routes";
+import ticketRouter from "../routes/ticket.routes";
+import ServerlessHttp from "serverless-http";
 
 dotenv.config();
 
@@ -29,6 +30,8 @@ app.use((err: any, _req: any, res: any, _next: any) => {
 const server = http.createServer(app);
 initSocket(server);
 
-server.listen(4000, () => {
-  console.log("🚀  http://localhost:4000");
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Vercel!" });
 });
+
+module.exports.handler = ServerlessHttp(app);
